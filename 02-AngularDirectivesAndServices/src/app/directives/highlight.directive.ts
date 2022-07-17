dca1edd78219593a783c7f1ef40b970878033b76
pramendraindian <pramendra.indian@gmail.com>
2022-07-17 15:49:34 +0530

    Custom directives live example

--- 02-AngularDirectivesAndServices/src/app/directives/highlight.directive.ts
+++ 02-AngularDirectivesAndServices/src/app/directives/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
+  @HostListener("mouseenter")
+  onMouseEnter() {
+    this.elementRef.nativeElement.style.backgroundColor = "yellow";
+  }
+
+  @HostListener("mouseleave")
+  onMouseLeave() {
+    this.elementRef.nativeElement.style.backgroundColor = "initial";
+  }
 
 }
