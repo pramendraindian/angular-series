1609e16c24b586c14cc6c6954e5f182a631fa42d
pramendraindian <pramendra.indian@gmail.com>
2022-07-24 08:35:08 +0530

    added pipe example

--- 02-AngularDirectivesAndServices/src/app/components/employee/employee.component.ts
+++ 02-AngularDirectivesAndServices/src/app/components/employee/employee.component.ts
@@ -1,17 +1,19 @@
 import { Component, Input, OnInit, Output,EventEmitter, OnChanges, SimpleChanges, OnDestroy, AfterViewInit, DoCheck } from '@angular/core';
 import { Employee } from 'src/app/models/Employee';
+import { DepartmentPipe } from 'src/app/pipes/department.pipe';
 
 
 @Component({
   selector: 'employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
+  providers:[DepartmentPipe]
   
 })
 export class EmployeeComponent implements OnInit,OnChanges,OnDestroy,AfterViewInit,DoCheck {
 @Input() employee:Employee= {} as Employee;
 @Output() empDetailsAdded:EventEmitter<void>=new EventEmitter()
-  constructor() { }
+  constructor(private deptPipe:DepartmentPipe) { }
   ngDoCheck(): void {
     console.warn('ngDoCheck fired');
   }
@@ -28,6 +30,7 @@ export class EmployeeComponent implements OnInit,OnChanges,OnDestroy,AfterViewIn
 
   ngOnInit(): void {
     console.warn('ngOnInit fired');
+    console.log(this.deptPipe.transform(1));
    
   }
   
