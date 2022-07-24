930300c50ee41a8ef4fdb5f5e4d4c33b3301d918
pramendraindian <pramendra.indian@gmail.com>
2022-07-24 20:05:39 +0530

    fork join batch optimization

--- 03-AngularServices/src/app/components/advanced/advanced.component.ts
+++ 03-AngularServices/src/app/components/advanced/advanced.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { forkJoin, Subject, merge, of, defer, concat, throwError, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map, filter, tap, takeLast, scan, startWith, mergeMap, finalize, ignoreElements, catchError } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, filter, tap, takeLast, scan, startWith, mergeMap, finalize, ignoreElements, catchError, delay } from 'rxjs/operators';
 @Component({
   selector: 'app-advanced',
   templateUrl: './advanced.component.html',
@@ -9,89 +9,111 @@ import { map, filter, tap, takeLast, scan, startWith, mergeMap, finalize, ignore
 })
 export class AdvancedComponent implements OnInit {
 
-  posts:unknown[]=[];
+  posts: unknown[] = [];
+  recordCount = 0;
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.callInBatch();
   }
 
- callInBatch() {
+  callInBatch() {
     const postIds = [];
-    for (let i = 498; i < 503; i++) {
+    const batchSize = 6;
+    for (let i = 1; i < 510; i++) {
       postIds.push(i);
     }
-
-    while (postIds?.length > 5) {
-      const curentBatch = postIds.splice(0, 6)
-      this.processSpecificBatch(curentBatch)
+    this.recordCount = postIds.length;
+    let batchNo = 0;
+    while (postIds?.length > 0) {
+      batchNo++;
+      const curentBatchSet = postIds.splice(0, batchSize)
+      console.warn(`Batch set = ${curentBatchSet}`)
+      this.processSpecificBatch(curentBatchSet, batchNo)
     }
-    if (postIds?.length > 0) {
-      this.processSpecificBatch(postIds);
-    }
-
+   
   }
 
-  processSpecificBatch(postIds: number[]) {
-    const result$ = this.getPostDetails(postIds);
+  processSpecificBatch(postIds: number[], batchNo: number) {
+    const result$ = this.getCommentDetails(postIds, batchNo);
     result$.pipe(
+      // OPTIMIZE IT WITH UNIT TESTING
+      delay(batchNo===1?0: batchNo*100), // Add a delay to avoid all the calls to be triggered in one go
       mergeMap(([finalResult, progress]) => merge(
         progress.pipe(
-          // tap((value) => {console.log(`${value} completed`)}),
-          ignoreElements()
+          tap((value) => { console.log(`Batch # ${batchNo} completed ${value} %`) }),
+          //ignoreElements() // DON'T EMIT PROGRESS BAR RESULTS
         ),
         finalResult
       ))
-    ).subscribe(batchCommentsResponse => {
-      // Add to global queue
-      
-      this.posts =[...this.posts,...batchCommentsResponse as unknown[]];
-      batchCommentsResponse
-      console.log(batchCommentsResponse);
-    
+    ).subscribe({
+      next: batchCommentsResponse => {
+        // Add to global queue
+        if (Array.isArray(batchCommentsResponse)) {
+          this.posts = [...this.posts, ...batchCommentsResponse as unknown[]];
+        }
+        // console.log(batchCommentsResponse);
+        if (this.recordCount === this.posts.length) {
+          console.warn('Final Response');
+          console.log(this.posts);
+        }
+
+      },
+      error: err => {
+
+      },
+      complete() {
+
+
+      },
+
+
+
     });
   }
-  
-  
-  getPostDetails(userIdsList: any[]) {
-    const arrayOfObservables = userIdsList.map((userId, index) => {
+
+
+  getCommentDetails(userIdsList: any[], batchNo: number) {
+    const requestList = userIdsList.map((userId, index) => {
       // if (index === 1) return of({message: 'Error Occured!'}); // testin with error
       return this.http.get('https://jsonplaceholder.typicode.com/comments/' + userId).pipe(
         //************************* Error Handling in fork Join****************************/
         // DO ERROR HANDLING AT THE INDIDUAL API CALL
         //// This is very very important to catch error to avoid killing observabel
-        catchError(err=>of({isError:true,error:err}))
-        );
+        catchError(err => of({ id: userId, isError: true, error: err })),
+
+      );
     }
     )
-    return this.forkJoinWithProgress(arrayOfObservables)
+    return this.forkJoinWithProgress(requestList, batchNo);
   }
-  
-  
-  forkJoinWithProgress(arrayOfObservables: Observable<any>[]) {
-    return  defer(() =>  {
+
+
+  forkJoinWithProgress(requestList: Observable<any>[], batchNo: number) {
+    return defer(() => {
       let counter = 0;
-      const percent$ = new Subject();
-      const modilefiedObservablesList = arrayOfObservables.map(
+      const percentProgres$ = new Subject();
+      const requestListWithProgress = requestList.map( // hookup progress indicator with each request
         (item, index) => item.pipe(
           tap((commentResponse) => {
             //console.log(commentResponse)
           }),
           finalize(() => {
-            const percentValue = ++counter * 100 / arrayOfObservables.length;
-            percent$.next(percentValue);
+            const percentValue = ++counter * 100 / requestList.length;
+            percentProgres$.next(percentValue);
           })
         )
       );
 
-      const finalResult$ =  forkJoin(modilefiedObservablesList).pipe(
+      const finalResult$ = forkJoin(requestListWithProgress).pipe(
         tap(() => {
-          percent$.next(100);
-          percent$.complete();
+          percentProgres$.next(100);
+          percentProgres$.complete();
         }
-        ));
+        )
+      );
 
-      return  of([finalResult$, percent$.asObservable()]);
+      return of([finalResult$, percentProgres$.asObservable()]);
     })
 
   }
