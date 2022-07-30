389c4f696e5e2cc61b2471ce29d37eadc52e50e2
pramendraindian <pramendra.indian@gmail.com>
2022-07-30 20:44:21 +0530

    Control browser alerts conditionally

--- 00-HTML-Tricks/src/app/app.component.ts
+++ 00-HTML-Tricks/src/app/app.component.ts
@@ -14,6 +14,8 @@ export class AppComponent implements OnInit {
 
   }
   ngOnInit(): void {
+    console.warn('on init');
+    this.browserService.isToNavigateWithoutWarning=false;
     this.subscribeToBrowserEvents();
   }
   subscribeToBrowserEvents()
