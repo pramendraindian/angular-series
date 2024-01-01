aa0ac0f526dea029479c7cc34c68e3a308c59e02
pramendraindian <pramendra.indian@gmail.com>
2024-01-01 19:27:22 +0530

    stand alone component documentation

--- 05-Angular-16/01-StandaloneAndSignal/src/app/app.component.ts
+++ 05-Angular-16/01-StandaloneAndSignal/src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component,computed,signal } from '@angular/core';
-import { CartComponent } from './components/cart/cart.component';
+import { Component} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CartComponent,RouterModule,HttpClientModule],
+  imports: [RouterModule],
   standalone: true
 })
 export class AppComponent {
   title = 'SignalDemoAng16';
-
+  
 }
--- 05-Angular-16/01-StandaloneAndSignal/src/app/components/home/home.component.ts
+++ 05-Angular-16/01-StandaloneAndSignal/src/app/components/home/home.component.ts
@@ -9,5 +9,66 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  appts=`
+import { Component} from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule],
+  standalone: true
+})
+export class AppComponent {
+  title = 'SignalDemoAng16';
+  
+}`;
+
+  maints=`
+  import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+  import { AppComponent } from './app/app.component';
+  import { provideRouter } from '@angular/router';
+  import { rootRoutes } from './app/app-routes';
+  import { HttpClientModule } from '@angular/common/http';
+  import { importProvidersFrom } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { CommonModule } from '@angular/common';
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(BrowserModule),
+      importProvidersFrom(FormsModule),
+      importProvidersFrom(CommonModule),
+      importProvidersFrom(HttpClientModule),
+      // you can pass all providers from your AppModule
+      provideRouter(rootRoutes),
+    ], 
+  }).catch(err => console.error(err));`;
+
+  routests=`
+  import { Routes } from '@angular/router';
+  import {HomeComponent } from './components/home/home.component';
+  
+  export const rootRoutes: Routes = [
+    {
+      path: '',
+      pathMatch: 'full',
+      component:HomeComponent 
+    },
+    {
+      path: 'cart',
+      loadComponent: () => import('./components/cart/cart.component')
+        .then(x => x.CartComponent)
+    },
+    {
+      path: 'poke',
+      loadComponent: () => import('./components/pokemon/pokemon.component')
+        .then(x => x.PokemonComponent)
+    },
+    {
+      path: '**',
+      redirectTo: '',
+    },
+  ];
+  `;
 
 }
